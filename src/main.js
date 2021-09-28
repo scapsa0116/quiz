48fcb2e2c5143358a743b9df57cf493f36ad6738
stela <stela.capsa@gmail.com>
2021-09-28 09:58:39 +0200

    building question form

---
+++ src/main.js
@@ -0,0 +1,46 @@
+const data =  [
+  {
+    question: "Capital of Moldova?",
+    a: "Chisinau",
+    b: "Bogota",
+    c: "Madrid",
+    d: "Istanbul",
+    correct: "a"
+  },
+
+  {
+    question: "Capital of Colombia",
+    a: "Chisinau",
+    b: "Bogota",
+    c: "Madrid",
+    d: "Istanbul",
+    correct: "b"
+  },
+  {
+    question: "How many continents are?",
+    a: "2",
+    b: "4",
+    c: "7",
+    d: "8",
+    correct: "c"
+  },
+  {
+    question: "Whats my name?",
+    a: "Juan",
+    b: "Stela",
+    c: "Elena",
+    d: "Auristela",
+    correct: "b"
+  },
+  {
+    question: "Latin language?",
+    a: "Romanian",
+    b: "Russian",
+    c: "Slovachian",
+    d: "Ukrainian",
+    correct: "a"
+  },
+
+  
+
+]
